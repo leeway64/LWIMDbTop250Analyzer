834d7808563e95808f541e1bf6c33805d35d50a3
Lee-Way Wang <lee.way56@comcast.net>
2022-05-28 23:12:15 -0700

    Create Base class, Success enum class, exitMessage function
    - Update README

--- src/CreatePlots.cpp
+++ src/CreatePlots.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 #include <yaml-cpp/yaml.h>
 
+
+// an enum class is like an enum, but strongly typed
+enum class Success
+{
+    success = 0
+};
+
+
+void exitMessage(Success s)
+{
+    if (s == Success::success)
+    {
+        std::cout << "IMDb Top 250 analyzed successfully" << std::endl;        
+    }
+}
+
+
 int main()
 {
     std::cout << "hello world" << std::endl;
+
+    Success S = Success::success;
+    exitMessage(S);
     return 0;
 }
 

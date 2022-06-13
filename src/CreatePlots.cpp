79d2abb0e87f5cd1937773e047fa50408c734fcf
Lee-Way Wang <lee.way56@comcast.net>
2022-06-12 21:30:39 -0700

    Remove actors, handle logic for CLI arguments
    - Remove actors who have appeared less than 5 times in the top 250
    - Create --help option

--- src/CreatePlots.cpp
+++ src/CreatePlots.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <exception>
 #include <filesystem>
 
 #include <yaml-cpp/yaml.h>
@@ -6,8 +7,29 @@
 #include "AnalyzerFunctions.hpp"
 
 
-int main()
+int main(int argc, char** argv)
 {
+    if (argc != 2)
+    {
+        throw std::invalid_argument("Invalid number of arguments. Run --help for more details.");
+    }
+
+    const std::string argument = argv[1];
+    const std::filesystem::path input_data_file{ argument };
+
+    if (argument == "--help")
+    {
+        AnalyzerFunctions::printHelp();
+        std::exit(EXIT_SUCCESS);
+    }
+    else if (!std::filesystem::exists(input_data_file))
+    {
+        throw std::invalid_argument("Input data file not found.");
+    }
+
+
+    // The new keyword dynamically allocates memory for an object and creates that object. It
+    // returns a pointer to that dynamically allocated object.
     Base * b = new Base();
     Base * sub = new Subclass();
 
@@ -47,12 +69,17 @@ int main()
         years_list.push_back(movie.second["year"].as<int>());
     }
 
-
     std::unordered_map<int, int> decades_count= AnalyzerFunctions::count_decades(years_list);
     std::unordered_map<std::string, int> genres_count = AnalyzerFunctions::count_genres(genres_list);
     std::unordered_map<double, int> ratings_count = AnalyzerFunctions::count_ratings(ratings_list);
-    std::unordered_map<std::string, int> cast_count = AnalyzerFunctions::count_cast(cast_list);
 
+    const int person_min_occurrences = 5;
+    std::unordered_map<std::string, int> cast_count = AnalyzerFunctions::count_cast(cast_list);
+    // Remove any person who hasn't appeared at least 5 times in the IMDb top 250
+    std::erase_if(cast_count, [person_min_occurrences](const auto& elem){
+        auto const& [key, value] = elem;
+        return value < person_min_occurrences;
+    });
     
     for (auto [key, value]: decades_count)
     {
@@ -71,17 +98,11 @@ int main()
 
     for (auto [key, value]: cast_count)
     {
-        
-        if (value >= 3)
-        {
             std::cout << key << " " << value << std::endl;
-        }
     }
 
-    AnalyzerFunctions::printHelp();
-    
     sub->message(S);  // Print exit message
-        
-    return 0;
+
+    std::exit(EXIT_SUCCESS);
 }
 
--- src/Subclass.cpp
+++ src/Subclass.cpp
@@ -11,7 +11,7 @@ Subclass::~Subclass(){
     std::cout << "\tRed Letter Media: https://www.youtube.com/user/RedLetterMedia" << std::endl;
     std::cout << "\tYourMovieSucksDOTorg: https://www.youtube.com/c/YourMovieSucksDOTorg" << std::endl;
     std::cout << "\tPony Smasher: https://www.youtube.com/sandberganimation (this is David F."
-                    "Sandberg's (director of Shazam) YouTube channel)" << std::endl;
+                    "Sandberg (director of Shazam)'s YouTube channel)" << std::endl;
     std::cout << "\tLindsay Ellis: https://www.youtube.com/c/LindsayEllisVids" << std::endl;
     std::cout << "\tLessons from the Screenplay: https://www.youtube.com/c/LessonsfromtheScreenplay" << std::endl;
     std::cout << "\tEvery Frame a Painting: https://www.youtube.com/c/everyframeapainting" << std::endl;
